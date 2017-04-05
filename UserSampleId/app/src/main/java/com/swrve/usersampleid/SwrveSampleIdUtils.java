8b29b9b240cd0fcfc78976df9eb73c6201f8524a
davidbreenwex <davidbreenwex@gmail.com>
2017-04-05 15:29:11 -0700

    modifications after code review

--- UserSampleId/app/src/main/java/com/swrve/usersampleid/SwrveSampleIdUtils.java
+++ UserSampleId/app/src/main/java/com/swrve/usersampleid/SwrveSampleIdUtils.java
@@ -15,30 +15,33 @@ import java.util.Map;
  *
  * config.setUserId("01a94aec-d8ba-4292-8ab3-04edef4d1d71"); // returns 1
  * config.setUserId("018bb009-db2c-4feb-9c61-ec5871cd1cea"); // returns 50
- * config.setUserId("01b2d2e6-3fa5-4abb-aad8-d0aed0ece9b8");   // returns 100
+ * config.setUserId("01b2d2e6-3fa5-4abb-aad8-d0aed0ece9b8"); // returns 100
  */
 
 public class SwrveSampleIdUtils {
 
     public static void sendSampleIdForUser() {
-        String user_id = SwrveSDK.getUserId();
+        String userId = SwrveSDK.getUserId();
 
-        // get md5 hash of the user_id
-        String hash = SwrveHelper.md5(user_id);
+        // get md5 hash of the userId
+        String hash = SwrveHelper.md5(userId);
         hash = hash.substring(0, 8);
 
         // get a numeric representation of the hash
-        double user_number = (double)Long.parseLong(hash, 16);
-        final double max_md5 = 4294967295.0;
-        // divide by the max_md5 to return a decimal between 0 and 1
-        double user_range = user_number / max_md5;
+        double userNumber = (double)Long.parseLong(hash, 16);
+        final double maxMd5 = 4294967295.0;
+        // divide by the maxMd5 to return a decimal between 0 and 1
+        double userRange = userNumber / maxMd5;
         // From this decimal get an int from 1-100
-        int userSampleId = (int) Math.ceil(user_range * 100.0d);
+        int userSampleId = (int) Math.ceil(userRange * 100.0d);
 
         // Update the user user_sample_id user property in Swrve
         Map<String, String> attributes = new HashMap<String, String>();
         attributes.put("user_sample_id", String.valueOf(userSampleId));
         SwrveSDK.userUpdate(attributes);
+
+        // Force the user update to send immediately so the user property
+        // can be used as soon as possible to segment the user
         SwrveSDK.sendQueuedEvents();
     }
 }
